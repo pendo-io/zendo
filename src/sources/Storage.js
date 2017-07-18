b33b68d6106628083f2ad6168192937004c7b3f2
Brec Carson <brec@pendo.io>
2017-07-18 09:36:01 -0400

    working on storage abstraction

--- src/sources/Storage.js
+++ src/sources/Storage.js
@@ -1,17 +1,17 @@
 import Rx from 'rxjs';
-
-const getObsForKey = (key) => {}
-const saveObsForKey = (key, obs) => {}
-
-const StorageObserverFactory = (key) => {
-  if (!getObsForKey(key)) {
-    saveObsForKey(key, Rx.Observable.create((subscriber) => {
-
-    }));
-  }
-
-  return getObsForKey(key);
-};
+//
+// const getObsForKey = (key) => {}
+// const saveObsForKey = (key, obs) => {}
+//
+// const StorageObserverFactory = (key) => {
+//   if (!getObsForKey(key)) {
+//     saveObsForKey(key, Rx.Observable.create((subscriber) => {
+//
+//     }));
+//   }
+//
+//   return getObsForKey(key);
+// };
 
 const Storage = {
 };
